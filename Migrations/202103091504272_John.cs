b51c2ae2b32b4fa562ffc91770a59fc46ea77f84
dk.raw <hello@dkatsios.tk>
2021-03-09 17:32:42 +0200

    Add files via upload

---
+++ Migrations/202103091504272_John.cs
@@ -0,0 +1,40 @@
+﻿namespace preview.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class John : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Identities",
+                c => new
+                    {
+                        IdentityID = c.Int(nullable: false),
+                        Number = c.String(),
+                    })
+                .PrimaryKey(t => t.IdentityID)
+                .ForeignKey("dbo.Students", t => t.IdentityID)
+                .Index(t => t.IdentityID);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.StudentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Identities", "IdentityID", "dbo.Students");
+            DropIndex("dbo.Identities", new[] { "IdentityID" });
+            DropTable("dbo.Students");
+            DropTable("dbo.Identities");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace preview.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<preview.MyDatabase>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(preview.MyDatabase context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
